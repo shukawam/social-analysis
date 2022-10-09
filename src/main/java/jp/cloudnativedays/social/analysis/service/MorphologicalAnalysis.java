9348051cf93d69f2719c207a5cf93b95e3c22a9c
shukawam <shukawam@gmail.com>
2022-10-09 13:10:26 +0900

    Fixed tokenize

--- src/main/java/jp/cloudnativedays/social/analysis/service/MorphologicalAnalysis.java
+++ src/main/java/jp/cloudnativedays/social/analysis/service/MorphologicalAnalysis.java
@@ -5,12 +5,16 @@ import com.atilika.kuromoji.ipadic.Tokenizer;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MorphologicalAnalysis {
 
 	public List<Token> getToken(String in) {
 		Tokenizer tokenizer = new Tokenizer();
+		List<String> tokens = tokenizer.tokenize(in).stream().map(token -> token.getPartOfSpeechLevel1())
+				.collect(Collectors.toList());
+		tokens.forEach(token -> System.out.println(token));
 		return tokenizer.tokenize(in);
 	}
 
